1569b03ae5733014e25e3350d19a0f461c5fb244
Aus Gomez <austreberto.gomez@capgroup.com>
2021-04-08 16:03:25 -0500

    Initial Commit

---
+++ FetchTacos/FetchTacosApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchTacosApp.swift
+//  FetchTacos
+//
+//  Created by Austreberto Gomez on 4/8/21.
+//
+
+import SwiftUI
+
+@main
+struct FetchTacosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
